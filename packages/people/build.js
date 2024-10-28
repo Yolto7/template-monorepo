e3ec343132912f4d14f1c7a1d7da966a5567b7dc
joaquin <joaquin.torres@ext.konecta-group.com>
2024-10-27 19:12:47 -0500

    .

---
+++ packages/people/build.js
@@ -0,0 +1,17 @@
+const esbuild = require('esbuild');
+const { dependencies } = require('./package.json');
+
+const sharedDeps = Object.keys(dependencies);
+
+const buildPackage = async (entry, outdir) => {
+  await esbuild.build({
+    entryPoints: [entry],
+    outdir,
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    external: sharedDeps,
+  });
+};
+
+buildPackage('src/**/*.ts', 'dist');
---
+++ packages/shared/build.js
@@ -0,0 +1,20 @@
+const esbuild = require('esbuild');
+const { dtsPlugin } = require('esbuild-plugin-d.ts');
+
+const { dependencies } = require('./package.json');
+
+const sharedDeps = Object.keys(dependencies);
+
+const buildPackage = async (entry, outdir) => {
+  await esbuild.build({
+    entryPoints: [entry],
+    outdir,
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    plugins: [dtsPlugin({})],
+    external: sharedDeps,
+  });
+};
+
+buildPackage('src/**/*.ts', 'dist');
